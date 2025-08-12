1b4e5775d3c8b0bcd1cce2abc575bfe49805c794
PandaArt <alexeyhaldeev@icloud.com>
2025-08-12 16:31:22 +0300

    DoTween and TMP imported

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,46 +6,28 @@ using System.Threading.Tasks;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _distance;
+    [SerializeField] private float _distance; 
     [SerializeField] private float _speed;
 
-    private CancellationTokenSource _tokenSource;
+    private readonly CancellationTokenSource _tokenSource = new();
 
-
-    private void Start()
+    private void OnDisable()
     {
-        _tokenSource = new CancellationTokenSource();
+        StopAllCoroutines();
     }
 
-    private void OnDestroy()
+    public void MoveTo(Vector3 targetPos)
     {
-        //StopAllCoroutines();
-    }
-
-    public async void MoveTo(Vector3 targetPos)
-    {
-        // StopAllCoroutines();
-        // StartCoroutine(Move(targetPos));
-        await MoveAsync(targetPos);
+        StopAllCoroutines();
+        StartCoroutine(Move(targetPos));
     }
     private IEnumerator Move(Vector3 targetPos)
     {
         while (Vector3.Distance(transform.position, targetPos) > _distance)
         {
-            transform.position = Vector3.Lerp(transform.position, targetPos, _speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, _speed * Time.deltaTime);
             yield return null;
         }
 
-    }
-    
-    private async Task MoveAsync(Vector3 targetPos)
-    {
-        await new Task(() =>
-        {
-            while (Vector3.Distance(transform.position, targetPos) > _distance)
-            {
-                transform.position = Vector3.Lerp(transform.position, targetPos, _speed * Time.deltaTime);
-            }
-        }, _tokenSource.Token);
     }
 }
